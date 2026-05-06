65304925d6397aa44138325fc3d0292dc68d8a6e
sudarshan <sudarshanpandat.com>
2026-05-06 13:31:42 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,10 @@ const cors=require('cors');
 
 const port = process.env.PORT || 4000;
 
-app.use(cors(
- origin:"http://localhost:5173/"
-))
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 
 
